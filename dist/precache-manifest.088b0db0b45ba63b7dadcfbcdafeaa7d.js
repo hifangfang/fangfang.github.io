f86f83ffe677673f38d4122e043e19101d02e5ae
hucf <15638975163@163.com>
2023-07-19 14:52:26 +0800

    dist文件

---
+++ dist/precache-manifest.088b0db0b45ba63b7dadcfbcdafeaa7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d22e452845e69b860e6644bb8cec03",
+    "url": "/config.js"
+  },
+  {
+    "revision": "21d33e70e2cc7d88f99e9f593ef51c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669631bfadddd0ffe1ea5c15a85174d5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d899bb7c64e354ede2ad",
+    "url": "/static/css/app.cccad52f.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "f10f4355169da147fcf4fae539cbbf76",
+    "url": "/static/img/ztgx_logo.f10f4355.svg"
+  },
+  {
+    "revision": "d899bb7c64e354ede2ad",
+    "url": "/static/js/app.3ea235fe.js"
+  },
+  {
+    "revision": "1be6e9f05152228eddef",
+    "url": "/static/js/chunk-vendors.73693276.js"
+  },
+  {
+    "revision": "af60e7209ad37a0cc23f0b492e2fd0cb",
+    "url": "/static/media/loginmp4.af60e720.mp4"
+  }
+]);
